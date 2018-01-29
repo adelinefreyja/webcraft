5f5401715009c9a3d67ebc9dc69d552eef4f51b3
OPEKA <opk3494@gmail.com>
2018-01-29 17:01:32 +0100

    Ajout controller view pour site vitrine (produits, commandes, Paramètre=> ecommerce

---
+++ src/Controller/OrdersManageController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\WebsiteInfo;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class OrdersManageController extends Controller
+{
+	/**
+	* @Route("/craft/manageorder", name="manageorder")
+	*/
+	public function new(Request $request) {
+
+        $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+        $query = $repository->findOneBy(
+            ["sitetype" =>  "2"]
+        );
+
+        return $this->render('backoffice/orders/manageorder.html.twig',
+            ["sitetype" =>  $query]
+        );
+	}
+}
