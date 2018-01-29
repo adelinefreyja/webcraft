6976bdff685de3ee99e29b0fff833d7da4030856
Erykyu <33733919+Erykyu@users.noreply.github.com>
2018-01-29 11:12:53 +0100

    Sélection des manusen fonction du type de site

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\WebsiteInfo;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +12,16 @@ class AdminDashboardController extends Controller
 	/**
 	* @Route("/craft", name="dashboard")
 	*/
-	public function new(Request $request)
-	{
+	public function new(Request $request) {
 
-        return $this->render('backoffice/dashboard.html.twig');
+	    $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+	    $query = $repository->findOneBy(
+	        ["sitetype" =>  "2"]
+        );
+
+        return $this->render('backoffice/dashboard.html.twig',
+            ["sitetype" =>  $query]
+        );
 	}
 
 		public function index()
--- src/Controller/NewPageController.php
+++ src/Controller/NewPageController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\WebsiteInfo;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,9 +11,15 @@ class NewPageController extends Controller
 	/**
 	* @Route("/craft/newpage", name="newpage")
 	*/
-	public function new(Request $request)
-	{
+	public function new(Request $request) {
 
-        return $this->render('backoffice/pages/addpage.html.twig');
+        $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+        $query = $repository->findOneBy(
+            ["sitetype" =>  "2"]
+        );
+
+        return $this->render('backoffice/pages/addpage.html.twig',
+            ["sitetype" =>  $query]
+        );
 	}
 }
