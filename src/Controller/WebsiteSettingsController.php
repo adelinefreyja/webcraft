1daec2c6c2f825e457ec00af4d77a8d5dda0b129
Adeline Freyja <freyjadesign@gmail.com>
2018-01-30 12:35:10 +0100

    Paramètres du site

--- src/Controller/WebsiteSettingsController.php
+++ src/Controller/WebsiteSettingsController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\WebsiteInfo;
+use App\Form\WebsiteModifyInfoType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,15 +12,28 @@ class WebsiteSettingsController extends Controller
 	/**
 	* @Route("/craft/settings/website", name="websitesettings")
 	*/
-	public function new(Request $request) {
+	public function websiteSettingsAction(Request $request) {
 
         $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
         $query = $repository->findOneBy(
             ["sitetype" =>  "2"]
         );
 
-        return $this->render('backoffice/settings/websitesettings.html.twig',
-            ["sitetype" =>  $query]
-        );
+        $website = $repository->findOneBy(['description' => 'siteinfo']);
+        $form = $this->createForm(WebsiteModifyInfoType::class, $website);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->redirectToRoute('websitesettings');
+        }
+
+        return $this->render('backoffice/settings/websitesettings.html.twig', array("sitetype" =>  $query, "form" => $form->createView(), 'website' => $website));
+
+
+        // return new Response($website->getOptionname(), $website->getOptionValue);
 	}
 }
---
+++ src/Form/WebsiteModifyInfoType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use App\Entity\WebsiteInfo;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WebsiteModifyInfoType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $builder
+            ->add("optionname", TextType::class,["label"    =>  "Nom du Site"])
+            ->add("optionvalue", TextType::class,["label"    =>  "Description"])
+            ->add("sitetype", ChoiceType::class,
+                [
+                    "label" => "Type de Site",
+                    "choices"   =>  [
+                        "E-commerce" =>  "2",
+                        "Vitrine" =>  "1"
+                    ],
+                    "expanded"  =>  true
+                ]
+            )
+            ->add("description", HiddenType::class,
+                ["data"   =>  "siteinfo"]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+
+        $resolver->setDefaults(
+            ['data_class' => WebsiteInfo::class]
+        );
+    }
+}
