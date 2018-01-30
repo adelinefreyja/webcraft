683e66a63d419456ebb1b867b9a812ba6b29d4c7
Adeline Freyja <freyjadesign@gmail.com>
2018-01-30 11:05:17 +0100

    User Account
    fixed modal bug
    working update form

---
+++ src/Controller/UserAccountController.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\WebsiteInfo;
+use App\Form\UserModifyInfoType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserAccountController extends Controller
+{
+	/**
+	* @Route("/craft/account", name="user_account")
+	*/
+	public function accountAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
+
+		$repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+        $query = $repository->findOneBy(
+            ["sitetype" =>  "2"]
+        );
+
+        $user = $this->getUser();
+        $form = $this->createForm(UserModifyInfoType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute('user_account');
+    	}
+
+        return $this->render('backoffice/user/account.html.twig', array("sitetype" =>  $query, "form" => $form->createView()));
+	}
+
+	public function index()
+	{
+		$this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+    	$user = $this->getUser();
+	    return new Response('Bienvenue, '.$user_first_name->getUserFirstName());
+	}
+}
