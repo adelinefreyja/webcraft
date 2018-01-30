c1a0d36b08eb94da59b62335d1c44fcd5c081ff9
OPEKA <opk3494@gmail.com>
2018-01-30 17:24:29 +0100

    gérer les commandes

--- src/Controller/OrdersManageController.php
+++ src/Controller/OrdersManageController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\WebsiteInfo;
+use App\Entity\Orders;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,13 +14,17 @@ class OrdersManageController extends Controller
 	*/
 	public function new(Request $request) {
 
-        $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
-        $query = $repository->findOneBy(
-            ["sitetype" =>  "2"]
-        );
+				$repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+				$query = $repository->findOneBy(
+						["sitetype" =>  "2"]
+				);
 
-        return $this->render('backoffice/orders/manageorders.html.twig',
-            ["sitetype" =>  $query]
-        );
+				$queryOrders = $this->getDoctrine()->getManager()->getRepository(Orders::class);
+				$orders = $queryOrders->findAll();
+
+				return $this->render('backoffice/orders/manageorders.html.twig',
+						[ "orders" => $orders,
+						"sitetype" =>  $query]
+				);
 	}
 }
