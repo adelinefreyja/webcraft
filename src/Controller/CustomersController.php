dce45f1fcf64d5adeff7770b6929d355b0a82442
OPEKA <opk3494@gmail.com>
2018-01-31 10:12:54 +0100

    Ajout Mes clients

--- src/Controller/CustomersController.php
+++ src/Controller/CustomersController.php
@@ -2,6 +2,9 @@
 namespace App\Controller;
 
 use App\Entity\WebsiteInfo;
+use App\Entity\Customers;
+use App\Entity\User;
+use App\Entity\UserAddress;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,9 +20,20 @@ class CustomersController extends Controller
         $query = $repository->findOneBy(
             ["sitetype" =>  "2"]
         );
+				$queryCustomers = $this->getDoctrine()->getManager()->getRepository(Customers::class);
+        $customers = $queryCustomers->findAll();
+
+				$queryUser = $this->getDoctrine()->getManager()->getRepository(User::class);
+        $user = $queryUser->findAll();
+
+				$queryUserAddress = $this->getDoctrine()->getManager()->getRepository(UserAddress::class);
+        $userAddress = $queryUserAddress->findAll();
 
         return $this->render('backoffice/customers/customers.html.twig',
-            ["sitetype" =>  $query]
+            [ "customers" => $customers,
+						  "user" => $user,
+							"userAddress" => $userAddress,
+							"sitetype" =>  $query]
         );
 	}
 }
