d8d18bef0ca44b7fbdf48714cab32e93b0fa1cd8
Adeline Freyja <freyjadesign@gmail.com>
2018-02-01 22:26:43 +0100

    Options
    Le controller qui va avec

--- src/Controller/OptionsController.php
+++ src/Controller/OptionsController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Entity\WebsiteInfo;
+use App\Entity\Pages;
+use App\Form\ContactOptionType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,15 +13,47 @@ class OptionsController extends Controller
 	/**
 	* @Route("/craft/options", name="options")
 	*/
-	public function new(Request $request) {
+	public function displayOptionsAction(Request $request) {
 
         $repository = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
         $query = $repository->findOneBy(
             ["sitetype" =>  "2"]
         );
 
+        $user = $this->getUser();
+        $user->getId();
+
+        $rep = $this->getDoctrine()->getManager()->getRepository(Pages::class);
+        $pages = $rep->findAll();
+
+        $fetchContactMod = $this->getDoctrine()->getManager()->getRepository(WebsiteInfo::class);
+        $contactMod = $fetchContactMod->findOneBy(
+            ["optionname" => "contact",
+            "description" => "module"
+            ]
+        );
+
+        $contactMod = new WebsiteInfo();
+        $activContact = $this->createForm(ContactOptionType::class, $contactMod);
+
+        $activContact->handleRequest($request);
+        if ($activContact->isSubmitted() && $activContact->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contactMod);
+            $em->flush();
+            $this->addFlash(
+                'success',
+                "Module activé !"
+            );
+            return $this->redirectToRoute('options');
+        }
+
+
         return $this->render('backoffice/customs/options.html.twig',
-            ["sitetype" =>  $query]
+            ["sitetype" =>  $query, "activContact" => $activContact->createView(), "contact" => $contactMod]
         );
 	}
+
+
 }
